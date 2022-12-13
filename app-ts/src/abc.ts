40d629187c322dfd74b061e48d785fad57d99c9e
Koda <elon.guo@gmail.com>
2022-12-13 22:43:18 +0800

    refactor

--- app-ts/src/abc.ts
+++
@@ -1,7 +0,0 @@
-import tsLib, { Pair } from '@guoguolong/lib-ts';
-
-const channel: Pair = tsLib.constants.channel.WECHAT;
-console.log('Channel: ', channel);
-
-window.martCtx = 'mars'
-console.log(globalPool)
--- app-ts/webpack.config.js
+++ app-ts/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/abc.ts',
+  entry: './src/index.ts',
   module: {
     rules: [
       {
